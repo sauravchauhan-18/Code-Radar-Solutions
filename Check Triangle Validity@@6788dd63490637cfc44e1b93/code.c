5abf6aa9f7f27cc246c49df712b5687bf79e3845
sauravchauhan-18 <sarjuchavan59@gmail.com>
2025-02-14 00:09:35 +0530

    Solution to Check Triangle Validity

--- Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
+++ Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
@@ -1,14 +1,33 @@
-#include<stdio.h>
+// #include<stdio.h>
 
-int main(){
-    int a,b,c;
-    scanf("%d %d %d",&a,&b,&c);
+// int main(){
+//     int a,b,c;
+//     scanf("%d %d %d",&a,&b,&c);
 
-    if((a+b+c)==180){
-        printf("Valid\n");
+//     if((a+b+c)==180){
+//         printf("Valid\n");
+//     }
+//     else{
+//         printf("Invalid\n");
+//         }
+//         return 0;
+// }
+
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    
+    // Taking input for three angles
+    printf("Enter three angles: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    // Checking if angles are positive and their sum is exactly 180
+    if (a > 0 && b > 0 && c > 0 && (a + b + c) == 180) {
+        printf("Valid Triangle\n");
+    } else {
+        printf("Invalid Triangle\n");
     }
-    else{
-        printf("Invalid\n");
-        }
-        return 0;
+
+    return 0;
 }
