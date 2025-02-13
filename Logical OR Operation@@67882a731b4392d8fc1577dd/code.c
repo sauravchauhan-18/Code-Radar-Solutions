99393a3dfa243af3aa3b8666ac1b6d8954376073
sauravchauhan-18 <sarjuchavan59@gmail.com>
2025-02-13 13:12:35 +0530

    Solution to Logical OR Operation

--- Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
+++ Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
@@ -1,19 +1,46 @@
-#include<stdio.h>
+// #include<stdio.h>
 
-int main(){
+// int main(){
+//     int a;
+//     int b;
+//     scanf("%d",&a);
+//     scanf("%d",&b);
+
+//     if(a>0 || b<0){
+//         printf("True");
+//     }
+//      else if(a==b || -a==-b){
+//         printf("False");
+//     }
+//      else if(a<b){
+//         printf("True");
+//     }
+//     return 0;
+// }
+
+
+
+#include <stdio.h>
+
+int main() {
     int a;
     int b;
-    scanf("%d",&a);
-    scanf("%d",&b);
 
-    if(a>0 || b<0){
-        printf("True");
+    // Read two integers from the user
+    printf("Enter two integers: ");
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    // Check conditions
+    if (a > 0 || b < 0) {
+        printf("True\n");
+    } else if (a == b || -a == -b) {
+        printf("False\n");
+    } else if (a < b) {
+        printf("True\n");
+    } else {
+        printf("No condition met\n"); // Optional: handle cases not covered
     }
-     else if(a==b || -a==-b){
-        printf("False");
-    }
-     else if(a<b){
-        printf("True");
-    }
+
     return 0;
 }
